b98f0899a6f0f35c6e5ad86394485af3b9c7236b
shixing_feng <shixing_feng@hotpoor.org>
2018-09-14 20:56:04 +0800

    搬移至webserver上运行，通过API接口调用

---
+++ webserver/three.py
@@ -0,0 +1,74 @@
+#/usr/bin/env/ python
+#coding=utf8
+
+import tornado.ioloop
+import tornado.web
+import httplib
+import md5
+import urllib
+import random
+from tornado.escape import json_decode
+
+# 临时接口
+class ThreeJSHandler_learn(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/05-hemisphere-light.html")
+
+# coffee重写部分
+
+class ThreeJSHandler_demo1_3(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_1.3.html")
+
+class ThreeJSHandler_demo1_5(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_1.5.html")
+
+class ThreeJSHandler_demo1_6(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_1.6.html")
+
+class ThreeJSHandler_demo1_8(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_1.8.html")
+
+class ThreeJSHandler_demo2_1(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.1.html")
+
+class ThreeJSHandler_demo2_12(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.12.html")
+
+class ThreeJSHandler_demo2_2(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.2.html")
+
+class ThreeJSHandler_demo2_22(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.22.html")
+
+class ThreeJSHandler_demo2_3(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.3.html")
+
+class ThreeJSHandler_demo2_32(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_2.32.html")
+
+class ThreeJSHandler_demo3_2(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_3.2.html")
+
+class ThreeJSHandler_demo3_22(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_3.22.html")
+
+class ThreeJSHandler_demo3_23(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_3.23.html")
+class ThreeJSHandler_demo3_24(tornado.web.RequestHandler):
+    def get(self):
+        self.render("template/demo_3.24.html")
+
+
---
+++ webserver/web.py
@@ -0,0 +1,50 @@
+#/usr/bin/env/ python
+#coding=utf8
+import os
+import tornado.ioloop
+import tornado.web
+import httplib
+import md5
+import urllib
+import random
+from tornado.escape import json_decode
+import three
+
+
+settings ={
+	"debug" : True,
+	"static_path" : os.path.join(os.path.dirname(__file__),"static")
+}
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, w2sss44")
+
+def make_app():
+    return tornado.web.Application([
+        # coffee重写例子
+        (r"/threejs1_3",three.ThreeJSHandler_demo1_3),
+        (r"/threejs1_5",three.ThreeJSHandler_demo1_5),
+        (r"/threejs1_6",three.ThreeJSHandler_demo1_6),
+        (r"/threejs1_8",three.ThreeJSHandler_demo1_8),
+        (r"/threejs2_1",three.ThreeJSHandler_demo2_1),
+        (r"/threejs2_12",three.ThreeJSHandler_demo2_12),
+        (r"/threejs2_2",three.ThreeJSHandler_demo2_2),
+        (r"/threejs2_22",three.ThreeJSHandler_demo2_22),
+        (r"/threejs2_3",three.ThreeJSHandler_demo2_3),
+        (r"/threejs2_32",three.ThreeJSHandler_demo2_32),
+        (r"/threejs3_2",three.ThreeJSHandler_demo3_2),
+        (r"/threejs3_22",three.ThreeJSHandler_demo3_22),
+        (r"/threejs3_23",three.ThreeJSHandler_demo3_23),
+        (r"/threejs3_24",three.ThreeJSHandler_demo3_24),
+
+        # 调用例子的临时接口
+        (r"/threejs_learn",three.ThreeJSHandler_learn),
+
+        (r"/", MainHandler),
+    ],**settings)
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
