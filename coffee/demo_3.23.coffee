1c671cb94693b50e672f9f81359f0802d7853a6b
shixing_feng <shixing_feng@hotpoor.org>
2018-09-14 15:07:01 +0800

    demo_3.23coffee重写 1.聚光源spotLight 2.target可以指向光源照射位置 3.PCFSoftShadowMap 更柔和的阴影

--- coffee/demo_3.23.coffee
+++ coffee/demo_3.23.coffee
@@ -1,4 +1,4 @@
-console.log "启用coffee demo_3.22"
+console.log "启用coffee demo_3.23"
 camera = null
 scene = null
 renderer = null
@@ -18,11 +18,11 @@ init = ()->
     renderer = new THREE.WebGLRenderer()
     renderer.setClearColor new THREE.Color 0xEEEEEE, 1.0
     renderer.setSize window.innerWidth, window.innerHeight
-    # renderer.shadowMapEnabled = true
+    renderer.shadowMapEnabled = true
     renderer.shadowMapType = THREE.PCFShadowMap
     
     # 创建地面方块
-    planeGeometry = new THREE.PlaneGeometry 60, 20, 20, 20
+    planeGeometry = new THREE.PlaneGeometry 60, 20, 1, 1
     planeMaterial = new THREE.MeshLambertMaterial {color:0xffffff}
     plane = new THREE.Mesh planeGeometry,planeMaterial
     
@@ -35,21 +35,36 @@ init = ()->
     scene.add plane
     
     # 基本光源
-    ambiColor = "#0c0c0c"
+    ambiColor = "#1c1c1c"
     ambientLight = new THREE.AmbientLight ambiColor
     scene.add ambientLight
 
     # 聚光源
-    spotLight = new THREE.SpotLight 0xffffff
+    spotLight0 = new THREE.SpotLight 0xcccccc
+    spotLight0.position.set -40, 30, -10
+    spotLight0.lookAt plane
+    scene.add spotLight0
+
+    target = new THREE.Object3D()
+    target.position = new THREE.Vector3 5, 0, 0
+
+    pointColor = "#ffffff"
+    spotLight = new THREE.SpotLight pointColor
     spotLight.position.set -40, 60, -10
     spotLight.castShadow = true
-    # scene.add spotLight
-
+    spotLight.shadowCameraNear = 2
+    spotLight.shadowCameraFar = 200
+    spotLight.shadowCameraFov = 30
+    spotLight.target = target
+    spotLight.distance = 0
+    spotLight.angle = 0.4
+    
+    scene.add spotLight
     #点光源
-    pointColor = "#ccffcc";
-    pointLight = new THREE.PointLight pointColor
-    pointLight.distance = 100
-    scene.add pointLight
+    # pointColor = "#ccffcc";
+    # pointLight = new THREE.PointLight pointColor
+    # pointLight.distance = 100
+    # scene.add pointLight
 
     # 无限光
     # directionalLight = new THREE.DirectionalLight 0xffffff, 0.7
@@ -58,7 +73,7 @@ init = ()->
 
     #创建方块，设置大小，位置
     cubeGeometry = new THREE.BoxGeometry 4, 4, 4
-    cubeMaterial = new THREE.MeshLambertMaterial {color: 0xff7777}
+    cubeMaterial = new THREE.MeshLambertMaterial {color: 0xff3333}
     cube = new THREE.Mesh cubeGeometry, cubeMaterial
     cube.castShadow = true
     cube.position.x = -4
@@ -84,9 +99,10 @@ init = ()->
     sphereLightMesh = new THREE.Mesh sphereLight, sphereLightMaterial
     sphereLightMesh.castShadow = true
 
-    sphereLightMesh.position = new THREE.Vector3 3, 0, 3
+    sphereLightMesh.position = new THREE.Vector3 3, 20, 3
     scene.add sphereLightMesh
 
+    stopMovingLight = false
     # 控制台
     controls = new ()->
         this.rotationSpeed = 0.03
@@ -94,19 +110,53 @@ init = ()->
         this.ambientColor = ambiColor
         this.pointColor = pointColor
         this.intensity = 1
-        this.distance = 100     
+        this.distance = 0
+        this.exponent = 30
+        this.angle = 0.1
+        this.debug = false
+        this.castShadow = true
+        this.onlyShadow = false
+        this.target = "Plane"
+        this.stopMovingLight = false   
     
     #控制条UI
     gui = new dat.GUI()
     gui.addColor(controls,"ambientColor").onChange (e)->
         ambientLight.color = new THREE.Color(e)
-    gui.addColor(controls, "pointColor").onChange (e)->
-        pointLight.color = new THREE.Color(e)
-    gui.add(controls, "intensity", 0, 3).onChange (e)->
-        pointLight.intensity = e
-    gui.add(controls, "distance", 0, 100).onChange (e)->
-        pointLight.distance = e
     
+    gui.addColor(controls, "pointColor").onChange (e)->
+        spotLight.color = new THREE.Color(e)
+    
+    gui.add(controls, "angle", 0, Math.PI * 2).onChange (e)->
+        spotLight.angle = e 
+    
+    gui.add(controls, "intensity", 0, 5).onChange (e)->
+        spotLight.intensity = e
+    
+    gui.add(controls, "distance", 0, 200).onChange (e)->
+        spotLight.distance = e
+    
+    gui.add(controls, "exponent", 0, 100).onChange (e)->
+        spotLight.exponent = e
+
+    gui.add(controls, "debug").onChange (e)->
+        spotLight.shadowCameraVisible = e
+
+    gui.add(controls, "castShadow").onChange (e)->
+        spotLight.castShadow = e
+
+    gui.add(controls, "onlyShadow").onChange (e)->
+        spotLight.onlyShadow = e
+
+    gui.add(controls, "target", ["Plane", "Sphere", "Cube"]).onChange (e)->
+        console.log(e)
+        switch (e)
+            when "Plane"  then spotLight.target = plane
+            when "Sphere" then spotLight.target = sphere
+            when "Cube"   then spotLight.target = cube
+
+    gui.add(controls, 'stopMovingLight').onChange (e)->
+        stopMovingLight = e
     # 实时渲染
     step = 0
     #灯光点动画变量 invert:倒置  phase:逐步执行
@@ -126,19 +176,21 @@ init = ()->
         sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(step)))
 
         #移动模拟小光点
-        if phase > 2 * Math.PI
-            invert = invert * -1
-            phase -= 2 * Math.PI
-        else
-            phase += controls.rotationSpeed
-        sphereLightMesh.position.z = +(7 * (Math.sin(phase)))
-        sphereLightMesh.position.x = +(14 * (Math.cos(phase)))
-        sphereLightMesh.position.y = 5
+        if not stopMovingLight
+            if phase > 2 * Math.PI
+                invert = invert * -1
+                phase -= 2 * Math.PI
+            else
+                phase += controls.rotationSpeed
+            sphereLightMesh.position.z = +(7 * (Math.sin(phase)))
+            sphereLightMesh.position.x = +(14 * (Math.cos(phase)))
+            sphereLightMesh.position.y = 10
 
-        if invert < 0
-            pivot = 14
-            sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot
-        pointLight.position.copy sphereLightMesh.position
+            if invert < 0
+                pivot = 14
+                sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot
+            
+        spotLight.position.copy sphereLightMesh.position
         
         requestAnimationFrame renderScene
         renderer.render scene,camera
--- js/demo_3.23.js
+++ js/demo_3.23.js
@@ -2,7 +2,7 @@
 (function() {
   var camera, controls, init, onResize, renderer, scene;
 
-  console.log("启用coffee demo_3.22");
+  console.log("启用coffee demo_3.23");
 
   camera = null;
 
@@ -13,7 +13,7 @@
   controls = null;
 
   init = function() {
-    var ambiColor, ambientLight, cube, cubeGeometry, cubeMaterial, gui, initStats, invert, phase, plane, planeGeometry, planeMaterial, pointColor, pointLight, renderScene, sphere, sphereGeometry, sphereLight, sphereLightMaterial, sphereLightMesh, sphereMaterial, spotLight, stats, step;
+    var ambiColor, ambientLight, cube, cubeGeometry, cubeMaterial, gui, initStats, invert, phase, plane, planeGeometry, planeMaterial, pointColor, renderScene, sphere, sphereGeometry, sphereLight, sphereLightMaterial, sphereLightMesh, sphereMaterial, spotLight, spotLight0, stats, step, stopMovingLight, target;
     // 场景
     scene = new THREE.Scene();
     // 摄像机
@@ -27,11 +27,11 @@
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.shadowMapEnabled = true
+    renderer.shadowMapEnabled = true;
     renderer.shadowMapType = THREE.PCFShadowMap;
     
     // 创建地面方块
-    planeGeometry = new THREE.PlaneGeometry(60, 20, 20, 20);
+    planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
     planeMaterial = new THREE.MeshLambertMaterial({
       color: 0xffffff
     });
@@ -44,20 +44,33 @@
     scene.add(plane);
     
     // 基本光源
-    ambiColor = "#0c0c0c";
+    ambiColor = "#1c1c1c";
     ambientLight = new THREE.AmbientLight(ambiColor);
     scene.add(ambientLight);
     // 聚光源
-    spotLight = new THREE.SpotLight(0xffffff);
+    spotLight0 = new THREE.SpotLight(0xcccccc);
+    spotLight0.position.set(-40, 30, -10);
+    spotLight0.lookAt(plane);
+    scene.add(spotLight0);
+    target = new THREE.Object3D();
+    target.position = new THREE.Vector3(5, 0, 0);
+    pointColor = "#ffffff";
+    spotLight = new THREE.SpotLight(pointColor);
     spotLight.position.set(-40, 60, -10);
     spotLight.castShadow = true;
-    // scene.add spotLight
-
+    spotLight.shadowCameraNear = 2;
+    spotLight.shadowCameraFar = 200;
+    spotLight.shadowCameraFov = 30;
+    spotLight.target = target;
+    spotLight.distance = 0;
+    spotLight.angle = 0.4;
+    scene.add(spotLight);
     //点光源
-    pointColor = "#ccffcc";
-    pointLight = new THREE.PointLight(pointColor);
-    pointLight.distance = 100;
-    scene.add(pointLight);
+    // pointColor = "#ccffcc";
+    // pointLight = new THREE.PointLight pointColor
+    // pointLight.distance = 100
+    // scene.add pointLight
+
     // 无限光
     // directionalLight = new THREE.DirectionalLight 0xffffff, 0.7
     // directionalLight.position.set -20, 40, 60
@@ -66,7 +79,7 @@
     //创建方块，设置大小，位置
     cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
     cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xff7777
+      color: 0xff3333
     });
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.castShadow = true;
@@ -92,8 +105,9 @@
     });
     sphereLightMesh = new THREE.Mesh(sphereLight, sphereLightMaterial);
     sphereLightMesh.castShadow = true;
-    sphereLightMesh.position = new THREE.Vector3(3, 0, 3);
+    sphereLightMesh.position = new THREE.Vector3(3, 20, 3);
     scene.add(sphereLightMesh);
+    stopMovingLight = false;
     // 控制台
     controls = new function() {
       this.rotationSpeed = 0.03;
@@ -101,7 +115,14 @@
       this.ambientColor = ambiColor;
       this.pointColor = pointColor;
       this.intensity = 1;
-      return this.distance = 100;
+      this.distance = 0;
+      this.exponent = 30;
+      this.angle = 0.1;
+      this.debug = false;
+      this.castShadow = true;
+      this.onlyShadow = false;
+      this.target = "Plane";
+      return this.stopMovingLight = false;
     };
     
     //控制条UI
@@ -110,15 +131,43 @@
       return ambientLight.color = new THREE.Color(e);
     });
     gui.addColor(controls, "pointColor").onChange(function(e) {
-      return pointLight.color = new THREE.Color(e);
+      return spotLight.color = new THREE.Color(e);
     });
-    gui.add(controls, "intensity", 0, 3).onChange(function(e) {
-      return pointLight.intensity = e;
+    gui.add(controls, "angle", 0, Math.PI * 2).onChange(function(e) {
+      return spotLight.angle = e;
     });
-    gui.add(controls, "distance", 0, 100).onChange(function(e) {
-      return pointLight.distance = e;
+    gui.add(controls, "intensity", 0, 5).onChange(function(e) {
+      return spotLight.intensity = e;
     });
-    
+    gui.add(controls, "distance", 0, 200).onChange(function(e) {
+      return spotLight.distance = e;
+    });
+    gui.add(controls, "exponent", 0, 100).onChange(function(e) {
+      return spotLight.exponent = e;
+    });
+    gui.add(controls, "debug").onChange(function(e) {
+      return spotLight.shadowCameraVisible = e;
+    });
+    gui.add(controls, "castShadow").onChange(function(e) {
+      return spotLight.castShadow = e;
+    });
+    gui.add(controls, "onlyShadow").onChange(function(e) {
+      return spotLight.onlyShadow = e;
+    });
+    gui.add(controls, "target", ["Plane", "Sphere", "Cube"]).onChange(function(e) {
+      console.log(e);
+      switch (e) {
+        case "Plane":
+          return spotLight.target = plane;
+        case "Sphere":
+          return spotLight.target = sphere;
+        case "Cube":
+          return spotLight.target = cube;
+      }
+    });
+    gui.add(controls, 'stopMovingLight').onChange(function(e) {
+      return stopMovingLight = e;
+    });
     // 实时渲染
     step = 0;
     //灯光点动画变量 invert:倒置  phase:逐步执行
@@ -137,20 +186,22 @@
       sphere.position.x = 20 + (10 * (Math.cos(step)));
       sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
       //移动模拟小光点
-      if (phase > 2 * Math.PI) {
-        invert = invert * -1;
-        phase -= 2 * Math.PI;
-      } else {
-        phase += controls.rotationSpeed;
+      if (!stopMovingLight) {
+        if (phase > 2 * Math.PI) {
+          invert = invert * -1;
+          phase -= 2 * Math.PI;
+        } else {
+          phase += controls.rotationSpeed;
+        }
+        sphereLightMesh.position.z = +(7 * (Math.sin(phase)));
+        sphereLightMesh.position.x = +(14 * (Math.cos(phase)));
+        sphereLightMesh.position.y = 10;
+        if (invert < 0) {
+          pivot = 14;
+          sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot;
+        }
       }
-      sphereLightMesh.position.z = +(7 * (Math.sin(phase)));
-      sphereLightMesh.position.x = +(14 * (Math.cos(phase)));
-      sphereLightMesh.position.y = 5;
-      if (invert < 0) {
-        pivot = 14;
-        sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot;
-      }
-      pointLight.position.copy(sphereLightMesh.position);
+      spotLight.position.copy(sphereLightMesh.position);
       requestAnimationFrame(renderScene);
       return renderer.render(scene, camera);
     };
